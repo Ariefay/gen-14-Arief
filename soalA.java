dd4dbfa72306d41325ab50a1c0d052f9fe1c63c8
Ariefay <125006355+Ariefay@users.noreply.github.com>
2023-02-22 21:42:22 +0700

    Tugas 2

---
+++ soalA.java
@@ -0,0 +1,28 @@
+package Matric;
+
+import java.util.*;
+
+public class soalA {
+
+	public static void main(String[] args) {
+
+		try (Scanner Input = new Scanner(System.in)) {
+			{
+				System.out.println("Silahkan masukan nilai N");
+				System.out.print("N=");
+				int n = Input.nextInt();
+
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+
+						System.out.print(" * ");
+					}
+					System.out.println(" ");
+				}
+
+			}
+		}
+
+	}
+
+}
